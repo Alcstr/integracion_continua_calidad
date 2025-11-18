9af0811f324485430bfc65cf4890ce3dd27355ec
Alcstr <johan.castro@campusucc.edu.co>
2025-11-18 07:46:41 -0500

    mathtest.tsx upload

---
+++ src/__tests__/math.test.tsx
@@ -0,0 +1,8 @@
+import { describe, it, expect } from "vitest";
+
+describe("Prueba básica inicial", () => {
+  it("Debería sumar correctamente", () => {
+    const a = 2 + 3;
+    expect(a).toBe(5);
+  });
+});
